92a13d8bbc81d8e5d1de9fbdaae241f61beecdcb
adiyaari01 <72090562+adiyaari01@users.noreply.github.com>
2021-03-04 15:16:32 +0200

    Add files via upload

---
+++ checkers/constants.py
@@ -0,0 +1,28 @@
+########### file of all the constats values ########### 
+import pygame
+
+WIDTH, HEIGHT = 700,700 #pixels 
+ROWS, COLS = 8,8 #number of rows and cols of board
+SQUARE_SIZE = WIDTH//COLS #size of each square in board
+
+#rgb
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GRAY = (128,128,128,128)
+
+##########################
+YELLOW = (255, 204, 0)
+#DELETE
+# LIGHT_BLUE = (51, 153, 255)
+# DARK_BLUE = (0, 38, 77)
+# PINK = (204, 0, 153)
+# DARK_PINK = (128, 0, 94)
+# LIGHT_PINK = (255, 179, 235)
+# ORANGE = (255, 140, 26)
+# PITCH = (255, 153, 153)
+##########################
+
+#bring and resize crown image
+CROWN = pygame.transform.scale(pygame.image.load('images/crown.png'),(40,21))
---
+++ checkers/piece.py
@@ -0,0 +1,51 @@
+from .constants import RED, WHITE, SQUARE_SIZE, GRAY, CROWN, GRAY
+import pygame
+
+class Piece:
+    PADDING = 15
+    OUTLINE = 2
+
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+        self.king = False
+        self.x = 0
+        self.y = 0
+        self.calculate_curr_pos()
+    
+    def calculate_curr_pos(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+
+    def change_to_king(self):
+        self.king = True
+    
+	#the following draws a piece on the board.
+    def draw(self, win,selected):
+        """ 
+        draw piece as a circle with the right color and gray border at the middle of square
+        """
+        radius = SQUARE_SIZE // 2 - self.PADDING
+        pygame.draw.circle(win, GRAY, (self.x, self.y), radius + self.OUTLINE)
+        pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+        #if selected:
+        #if self.color==RED:
+            #    pygame.draw.circle(win, (255,0,255), (self.x, self.y), radius)#(col * SQUARE_SIZE + SQUARE_SIZE//2, row * SQUARE_SIZE + SQUARE_SIZE//2), 8)
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
+
+    def move(self, row, col):
+        """
+        update piece curr_pos and call to calculate_curr_pos()
+        """
+        self.row = row
+        self.col = col
+        self.calculate_curr_pos()
+        
+    #def get_piece_color(self,row,col):
+     #   return self.color
+   
+#for debugging
+    def __repr__(self):
+        return str(self.color)
